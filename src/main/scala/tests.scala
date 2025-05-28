ab0fde787d0267244316d241f3f0a497332ee205
Alice <alexandervtarasovddd@gmail.com>
2025-05-29 00:12:32 +0300

    tests to tests

--- src/main/scala/tests.scala
+++ src/main/scala/tests.scala
@@ -6,7 +6,9 @@ import org.jgrapht.*
 import org.jgrapht.graph.*
 
 import java.net.URI
+import java.util.UUID
 import scala.jdk.CollectionConverters.*
+import scala.util.Random
 
 // jGraphT example
 @main def jGraphTest():Unit =
@@ -38,6 +40,128 @@ import scala.jdk.CollectionConverters.*
 
   println(s"cycledWorkstations(Seq(1,2,3)).take(10).toVector = ${cycledWorkstations(Seq(1,2,3)).take(10).toVector}")
 
+@main def eqTest():Unit = {
+
+  val videoshots = Seq(
+    VideoShot(UUID.randomUUID()),
+    VideoShot(UUID.randomUUID()),
+    VideoShot(UUID.randomUUID()),
+    VideoShot(UUID.randomUUID())
+  )
+  val signals = Seq(
+    Signal(UUID.randomUUID()),
+    Signal(UUID.randomUUID()),
+    Signal(UUID.randomUUID())
+  )
+
+  val mapping:SignalShotMapping = Map(
+    signals(0) -> videoshots.toSet,
+    signals(1) -> videoshots.take(3).toSet,
+    signals(2) -> videoshots.takeRight(2).toSet
+  )
+
+
+  val arms = Set(
+    ARM(
+      UUID.randomUUID(),
+      Set(Display(UUID.randomUUID()), Display(UUID.randomUUID())),
+      Set(videoshots(0), videoshots(1)),
+      Set(signals(0), signals(1)),
+    ),
+    ARM(
+      UUID.randomUUID(),
+      Set(Display(UUID.randomUUID())),
+      Set(videoshots(2), videoshots(3)),
+      Set(signals(0), signals(1), signals(2))
+    )
+  )
+
+
+  //case class VCluster(k: Int, vs: Set[VideoShot], ss: Set[Signal])
+
+  def indexShowMap[T](seq:Seq[T], prefix:String="", l:String="(", r:String = ")"):Map[T, String] =
+    seq.zipWithIndex.map{
+      case (x, i)=> x-> (prefix + l + i + r)
+    }.toMap
+
+  def formatCluster(cluster:VCluster, signals:Seq[Signal], videoshots: Seq[VideoShot]):String = {
+    val signalsIndexMapping = indexShowMap(signals, "s")
+    val videoshotsIndexMapping = indexShowMap(videoshots, "v")
+    s"VCluster(${cluster.k}, ${cluster.vs.map(videoshotsIndexMapping)}, ${cluster.ss.map(signalsIndexMapping)})"
+  }
+  def formatBlock(sb:ScenarioBlock, arms:Seq[ARM], signals:Seq[Signal], videoshots:Seq[VideoShot]):String = {
+    val armIndexMap = indexShowMap(arms, "arm")
+    val videoshotIndexMap = indexShowMap(videoshots, "v")
+    val signalIndexMap = indexShowMap(signals, "s")
+    val stepsString = sb.steps.map {
+      case ScenarioValidationStep(confId, arm, videoShot, ss) =>
+        s"Step($confId:${armIndexMap(arm)},${videoshotIndexMap(videoShot)},Signals(${ss.map(signalIndexMap).mkString(", ")})"
+    }.mkString("\n")
+    val configurationPrefixString = s"SetConfiguration(${sb.videoshotsConfiguration.id},"
+    val configurationMappingsString = sb.videoshotsConfiguration.vs.map {
+      case (arm, vs) =>
+        armIndexMap(arm) + "->" + vs.map(videoshotIndexMap).mkString("V{", ", ", "}")
+    }
+    val configurationPostfixString = ")"
+    val confShow = configurationPrefixString + configurationMappingsString + configurationPostfixString
+
+    confShow + "\n" + stepsString
+  }
+
+
+  val k = 3
+  val result = algLoop(k ,arms, mapping)
+  val scenarioShow = getSteps(result, arms)
+//  val result = getClustersOfSize(
+//    k, arms, mapping, Seq.empty
+//  )
+//  println(result)
+//  println(result.map(formatCluster(_, signals, videoshots)).mkString("\n"))
+//  println("#"*80)
+//  val result2 = getClustersOfSize(
+//    k-1, arms, mapping, result.toSeq
+//  )
+//  println(result2.map(formatCluster(_, signals, videoshots)).mkString("\n"))
+//  println("#" * 80)
+//  val result3 = getClustersOfSize(
+//    k - 2, arms, mapping, result.toSeq ++ result2.toSeq
+//  )
+//  println(result3.map(formatCluster(_, signals, videoshots)).mkString("\n"))
+  println(result.map(formatCluster(_, signals, videoshots)).mkString("\n"))
+  println("#"*90)
+  println(scenarioShow.map(formatBlock(_, arms.toSeq, signals, videoshots)).mkString("\n"))
+}
+
+def pick(n: Int, rangeLimit: Int) =
+  if (n > rangeLimit)
+    throw IllegalArgumentException(
+      s"An attempt to pick $n unique indices from 0 to $rangeLimit range"
+    )
+
+  def pickDifferent(chosen: Seq[Int], rangeLimit: Int): Int =
+    val candidate = Random.nextInt(rangeLimit)
+    if (!chosen.contains(candidate)) candidate
+    else pickDifferent(chosen, rangeLimit)
+
+  (1 to n).foldLeft(Seq.empty[Int])((chosen, _) =>
+    chosen.appended(pickDifferent(chosen, rangeLimit))
+  )
+
+case class TestedSystem(arms:Set[ARM], signals:Set[Signal], videoshots: Set[VideoShot], mapping:SignalShotMapping)
+def generateARMS( armN:Int,
+                  sigN:Int,
+                  vidN:Int,
+                  displayRange:(Int, Int),
+                  s2vN:Int,
+                  v2arm:Int,
+                ):TestedSystem ={
+  val signals = for (_<-0 to sigN) yield Signal(UUID.randomUUID())
+  val videoshots = for (_<-0 to sigN) yield VideoShot(UUID.randomUUID())
+  val mapping = signals.map{ s=>
+    s -> pick(s2vN, vidN).map(videoshots)
+  }
+  ???
+}
 
 
 
---
+++ src/main/scala/v3/ExpAlg.scala
@@ -0,0 +1,133 @@
+import java.util.UUID
+import scala.annotation.tailrec
+//
+
+type ID = UUID
+type SignalShotMapping = Map[Signal, Set[VideoShot]] // условно говоря, наша бд.
+
+case class Display(id:ID)
+
+case class VideoShot(id:ID)
+
+case class Signal(id:ID)
+
+case class ARM(id:ID, ds:Set[Display], vs:Set[VideoShot], ss:Set[Signal])
+
+case class VCluster(k:Int, vs:Set[VideoShot], ss:Set[Signal])
+
+case class ScenarioValidationStep( configurationId:Int,
+                                   arm:ARM,
+                                  //display:Display,
+                                  videoShot: VideoShot,
+                                  ss:Set[Signal])
+case class ScenarioBlock(videoshotsConfiguration: VideoshotsConfiguration, steps:Seq[ScenarioValidationStep])
+
+case class VideoshotsConfiguration(id:Int, vs:Map[ARM, Set[VideoShot]])
+
+case class Scenario(steps:Seq[ScenarioBlock])
+
+//Шаг 1. Сформировать список кластеров величины К.
+def getClustersOfSize(k:Int, arms:Set[ARM], signalShotMapping: SignalShotMapping, oldClusters: Seq[VCluster]):Set[VCluster] = {
+  //Возьмём все сигналы, отображаемые на k или более видеокадрах.
+  //Сформируем из них все возможные vcluster c одним сигналом и мощностью k
+  val clusters = for {
+    arm <- arms
+    (s, videoShots) <- signalShotMapping
+    if videoShots.size >= k
+    vs <- videoShots.subsets(k)
+    if arms.forall(testArm=> testArm.ds.size >= vs.intersect(testArm.vs).size)
+    if !oldClusters.exists(c=> vs.subsetOf(c.vs) && c.ss.contains(s))
+  } yield VCluster(k, vs, Set(s))
+
+  def mergeClustersStep(clusters: Set[VCluster]): Set[VCluster] = {
+    val newClusters = for {
+      vc1 <- clusters
+      vc2 <- clusters
+      if vc1 != vc2
+      if vc1.vs == vc2.vs
+    } yield ((vc1, vc2), VCluster(vc1.k, vc1.vs, vc1.ss ++ vc2.ss))
+    //return newClusters.map(_._2);
+    newClusters.map(c => c._2) ++ clusters.filterNot(c => newClusters.exists(nc => nc._1._1 == c || nc._1._2 == c))
+  }
+
+  @tailrec
+  def mergeClusters(clusters: Set[VCluster]): Set[VCluster] =
+    val next = mergeClustersStep(clusters)
+    if (next == clusters) clusters else mergeClusters(next)
+
+  mergeClusters(clusters)
+}
+
+////Шаг 2. Сортировка
+//def sortClusters(cs:Set[VCluster]):Seq[VCluster] = cs.toSeq.sortBy(c=>c.ss.size)
+
+//Шаг 12. Запись
+def writeScenario(steps:Seq[ScenarioBlock]):Unit =
+  println(steps)
+
+//Шаг 3. Сформировать шаги
+//todo: имитировать СИГНАЛЫ, выкинуть видеокадры.
+def getSteps(vClusters:Seq[VCluster], arms:Set[ARM]):Seq[ScenarioBlock] = {
+  vClusters.zipWithIndex.map { case (c, i)=>
+    val configuration = VideoshotsConfiguration(
+      i,
+      arms.map{ arm=>
+        arm -> arm.vs.intersect(c.vs)
+      }.toMap
+    )
+    val steps = configuration.vs.flatMap {
+      case (arm, vs) =>
+        vs.map(v=> ScenarioValidationStep(i, arm, v, c.ss))
+    }
+    ScenarioBlock(configuration, steps.toSeq)
+  }
+}
+
+//val k = arms.flatMap(_.ds).size
+def algLoop(k:Int, arms:Set[ARM], signalShotMapping: SignalShotMapping, clustersTested:Seq[VCluster] = Seq.empty):Seq[VCluster] =
+  if(k == 0) clustersTested
+  else
+    algLoop(
+      k - 1, 
+      arms, 
+      signalShotMapping, 
+      clustersTested.appendedAll(getClustersOfSize(k, arms, signalShotMapping, clustersTested))
+    )
+    
+
+  //???
+
+//def algorithmItself(arms:Set[ARM], signalShotMapping: SignalShotMapping):Unit = {
+//  //Шаг 0. k = |D|
+//  var k = arms.flatMap(_.ds).size
+//
+//  //Шаг 1. Сформировать список кластеров величины К.
+//  val clusters = getClustersOfSize(k, arms, signalShotMapping, Seq.empty)
+//  //Шаг 2. Сортировка
+//  val sortedClusters = sortClusters(clusters)
+//  //Шаг 3. Сформировать шаги
+//  val steps = getSteps(sortedClusters, arms)
+//  //Шаг 4. 𝑘 = 𝑘 − 1, ℎ = 1;
+//  //Что такое h?
+//
+//  //h : раз уж мы хотим иметь под рукой последний кофниг, просто будем его хранить.
+//  var lastConfig = steps.last.videoshotsConfiguration
+//  k = k - 1
+//  //Шаг 5. "Если 𝑘 > 0, то перейти к шагу 6, иначе перейти к шагу 12"
+//  //var writeFlag = true
+//  while (k > 0) {
+//    //Шаг 6 (и 8)
+//    val newClusters = getClustersOfSize(k, arms, signalShotMapping, clusters.toSeq)
+//    if (newClusters.nonEmpty) {
+//      //Шаги 8 - 10
+//      //Шаг 11
+//      k = k - 1
+//    } else {
+//      //Шаг 11
+//      k = k - 1
+//    }
+//  }
+//  //Шаг 12 Запись
+//  writeScenario(steps)
+//}
+
